d3f988d080e83b403b586d4ccd4b94ff41b652ae
Adarsh Hegde <ch22b048@smail.iitm.ac.in>
2024-07-03 10:25:17 +0530

    1 - setting up project

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+//express app created , stored in const app...
+const app = express()
+
+app.get('')
+
+
+//listen for requests
+app.listen(4000 , () => {
+    console.log("listening on Port 4000")
+})
+
